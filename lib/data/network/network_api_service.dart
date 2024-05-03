85ad89b46f36c2bcb80003bc8b2620edb4f6b5f7
ahmedkaimkhani <ak9302359@gmail.com>
2024-05-04 02:46:31 +0500

    Learning MVVM with Provider

--- lib/data/network/network_api_service.dart
+++ lib/data/network/network_api_service.dart
@@ -1,1 +1,51 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
+import 'package:mvvm_with_provider/data/app_exceptions.dart';
+import 'package:mvvm_with_provider/data/network/base_api_service.dart';
+
+class NetworkApiService extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(Uri.parse(url), body: data)
+          .timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body.toString());
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw FetchDataException(
+            'Error accrued while communicating with server' +
+                'with status code' +
+                response.statusCode.toString());
+    }
+  }
+}
