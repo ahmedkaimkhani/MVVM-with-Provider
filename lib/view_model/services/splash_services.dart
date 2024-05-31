4fc8a83b2147a8aeaa215d42dc3c5d4f78ba0125
ahmedkaimkhani <ak9302359@gmail.com>
2024-05-31 17:53:12 +0500

    Learning MVVM with Provider

--- lib/view_model/services/splash_services.dart
+++ lib/view_model/services/splash_services.dart
@@ -9,13 +9,16 @@ class SplashServices {
   Future<UserModel> getUserData() => UserViewModel().getUser();
 
   void checkAuthentication(BuildContext context) async {
-    getUserData().then((value) {
+    getUserData().then((value) async {
       if (value.token == 'null' || value.token == '') {
+        await Future.delayed(const Duration(seconds: 3));
         Navigator.pushNamed(context, RoutesName.login);
       } else {
+        await Future.delayed(const Duration(seconds: 3));
         Navigator.pushNamed(context, RoutesName.home);
       }
-    }).onError((error, stackTrace) {
+    }).onError((error, stackTrace) async {
+      await Future.delayed(const Duration(seconds: 3));
       if (kDebugMode) {
         print(error.toString());
       }
